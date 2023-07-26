fbe3116c6484e12624175bf8c0fa598855d9f744
Su Mon Aung <sumonaung575@gmail.com>
2023-07-26 21:59:23 +0630

    testing by changing some text in slider files

--- 07-slider/src/App.js
+++ 07-slider/src/App.js
@@ -3,7 +3,7 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 import { FaQuoteRight } from 'react-icons/fa';
 import data from './data';
 function App() {
-  return <h2>slider project setup</h2>;
+  return <h2>Postponing project as I stuck with git & github</h2>;
 }
 
 export default App;
