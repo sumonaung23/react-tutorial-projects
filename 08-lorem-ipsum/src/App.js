a4a631e6c5ccdd0a4cb8a341be5121b3b15bbb2f
Su Mon Aung <sumonaung575@gmail.com>
2023-07-30 12:07:10 +0630

    seted up form along with submit function and useState hook

--- 08-lorem-ipsum/src/App.js
+++ 08-lorem-ipsum/src/App.js
@@ -1,9 +1,27 @@
 import React, { useState } from 'react';
 import data from './data';
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello world.')
+  }
   
   return (
-    
+    <section className="section-center">
+      <h3>tired of typing boring lorem ipsum?</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+        <input type="number" name='amount' id='amount' value={count} onChange={(e) => setCount(e.target.value)} />
+        <button type='submit' className='btn'>generate</button>
+      </form>
+
+      
+    </section>
   );
 }
 
