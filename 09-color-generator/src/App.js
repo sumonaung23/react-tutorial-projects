a3477e98e3d4afbccecf230c5ef5002da9ffb79e
Su Mon Aung <sumonaung575@gmail.com>
2023-07-31 17:33:21 +0630

    modified handleSubmit function and colors section

--- 09-color-generator/src/App.js
+++ 09-color-generator/src/App.js
@@ -6,11 +6,18 @@ import Values from 'values.js'
 function App() {
   const [color, setColor] = useState('');
   const [error, setError] = useState(false);
-  const [list, useList] = useState([]);
+  const [list, setList] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello');
+    try {
+      let colors = new Values(color).all(10)
+      setList(colors);
+      console.log(colors);
+    } catch (error) {
+      setError(true)
+      console.log(error);
+    } 
   }
 
   return (
@@ -18,13 +25,22 @@ function App() {
       <section className="container">
         <h3>color generator</h3>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={color} onClick={(e) => setColor(e.target.value)} placeholder='#f15025' />
+          <input type="text" value={color} onChange={(e) => setColor(e.target.value)} placeholder='#f15025'
+          className={`${error ? 'error' : null}`} />
+
           <button className="btn" type='submit'>submit</button>
         </form>
       </section>
 
       <section className='colors'>
-        <h4>list goes here</h4>
+        {
+          list.map((color,index) => {
+            console.log(color)
+            return (
+              <SingleColor key={index} {...color} index={index} />
+            );
+          })
+        }
       </section>
     </>
   )
