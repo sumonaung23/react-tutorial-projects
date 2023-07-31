2a2d162b2e17f7eb14333c746926f5eb27ffbc80
Su Mon Aung <sumonaung575@gmail.com>
2023-08-01 01:56:00 +0630

    setted up list section using map

--- 10-grocery-bud/src/App.js
+++ 10-grocery-bud/src/App.js
@@ -11,7 +11,17 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log('hello');
+    if(!name) {
+      // display alert
+
+    } else if (name && isEditing) {
+      //deal with edit
+    } else {
+      // show alert
+      const newItem = {id: new Date().getTime().toString(),title:name};
+      setList([...list,newItem]);
+      setName('');
+    }
   }
 
   return (
@@ -27,8 +37,12 @@ function App() {
             </button>
           </div>
         </form>
-        <List />
-        <div className="clear-btn">clear items</div>
+        {list.length > 0 && 
+          <div className="grocery-container">
+            <List items={list} />
+            <button className="clear-btn">clear items</button>
+          </div>
+        }
       </div>
     </section>
   );
