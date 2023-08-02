e1f9e55ef3b485a66b3792e0af814edb19cd6488
Su Mon Aung <sumonaung575@gmail.com>
2023-08-02 23:28:53 +0630

    modified navbar with map method

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -11,45 +11,29 @@ const Navbar = () => {
         <button className="nav-toggle">
           <FaBars />
         </button>
-        <div className="links-container show-container">
-          <ul className="links">
-            <li>
-              <a href="#">home</a>
-            </li>
-            <li>
-              <a href="#">about</a>
-            </li>
-            <li>
-              <a href="#">contact</a>
-            </li>
-            <li>
-              <a href="#">products</a>
-            </li>
-          </ul>
-        </div>
-        <ul className="social-icons">
-            <li>
-              <a href="https://www.twitter.com">
-                <FaTwitter />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.twitter.com">
-                <FaTwitter />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.twitter.com">
-                <FaTwitter />
-              </a>
-            </li>
-            <li>
-              <a href="https://www.twitter.com">
-                <FaTwitter />
-              </a>
-            </li>
-          </ul>
       </div>
+      <div className="links-container show-container">
+        <ul className="links">
+          {
+            links.map((link) => {
+              const {id, url, text} = link;
+              return <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            })
+          }
+        </ul>
+      </div>
+      <ul className="social-icons">
+        {
+          social.map((socialIcon) => {
+            const {id, url, icon} = socialIcon;
+            return <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          })
+        }
+      </ul>
     </div>
   </nav>
 }
