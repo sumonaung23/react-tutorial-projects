0571f867947072fa1823e6871649aa0c5ab37fb9
Su Mon Aung <sumonaung575@gmail.com>
2023-08-02 23:49:52 +0630

    wrapped up with useEffect and useRef hooks

--- 11-navbar/src/Navbar.js
+++ 11-navbar/src/Navbar.js
@@ -4,16 +4,30 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if(showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`
+    } 
+    else{
+      linksContainerRef.current.style.height = '0px'
+    }
+  },[showLinks])
+
   return <nav>
     <div className="nav-center">
       <div className="nav-header">
         <img src={logo} alt='logo' />
-        <button className="nav-toggle">
+        <button className="nav-toggle" onClick={() => setShowLinks(!showLinks)}>
           <FaBars />
         </button>
       </div>
-      <div className="links-container show-container">
-        <ul className="links">
+      <div className="links-container" ref={linksContainerRef}>
+        <ul className="links" ref={linksRef}>
           {
             links.map((link) => {
               const {id, url, text} = link;
