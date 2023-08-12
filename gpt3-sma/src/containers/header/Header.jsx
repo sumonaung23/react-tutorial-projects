9fe584a3cb7126ebd29d9ef8ef8e625f94d134ee
Su Mon Aung <sumonaung575@gmail.com>
2023-08-13 00:30:51 +0630

    modified and styled header

--- gpt3-sma/src/containers/header/Header.jsx
+++ gpt3-sma/src/containers/header/Header.jsx
@@ -20,10 +20,10 @@ const Header = () => {
                     <img src={people} alt="" />
                     <p>1,600 people requested access a visit in last 24 hours</p>
                 </div>
-
-                <div className="gpt3__header-image">
-                    <img src={ai} alt="ai" />
-                </div>
+            </div>
+            
+            <div className="gpt3__header-image">
+                <img src={ai} alt="ai" />
             </div>
         </div>
     )
