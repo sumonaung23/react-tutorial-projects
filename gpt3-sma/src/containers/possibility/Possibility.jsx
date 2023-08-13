ff1673d26d9093e2941ce5bcefe73c3f94a82beb
Su Mon Aung <sumonaung575@gmail.com>
2023-08-13 22:46:08 +0630

    styled possibility section

--- gpt3-sma/src/containers/possibility/Possibility.jsx
+++ gpt3-sma/src/containers/possibility/Possibility.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import './possibility.css';
 import possibilityImage from '../../assets/possibility.png';
+import './possibility.css';
 
 const Possibility = () => {
     return (
-        <div className='gpt3__posiibility section__padding' id='possibility'>
+        <div className='gpt3__possibility section__padding' id='possibility'>
             <div className="gpt3__possibility-image">
                 <img src={possibilityImage} alt="possibility" />
             </div>
             <div className="gpt3__possibility-content">
                 <h4>Request Early Access to Get Started</h4>
-                <h1 className="gradient__text">
-                    The possibilities are beyond your imagination
-                </h1>
+                <h1 className="gradient__text">The possibilities are <br /> beyond your imagination</h1>
                 <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
                 <h4>Request Early Access to Get Started</h4>
             </div>
@@ -20,4 +18,4 @@ const Possibility = () => {
     )
 }
 
-export default Possibility
+export default Possibility;
