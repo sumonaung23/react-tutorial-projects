8653e32ffaa54ef115a1e395a5fa9de49716f5c7
Su Mon Aung <sumonaung575@gmail.com>
2023-08-14 00:42:21 +0630

    structured article section including props

--- gpt3-sma/src/containers/blog/Blog.jsx
+++ gpt3-sma/src/containers/blog/Blog.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import Article from '../../components'
+import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './import'
 import './blog.css';
 
 const Blog = () => {
@@ -10,13 +11,16 @@ const Blog = () => {
             </div>
             <div className="gpt3__blog-container">
                 <div className="gpt3__blog-container_groupA">
-                    <Article />
+                    <Article imgUrl={blog01} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
                 </div>
                 <div className="gpt3__blog-container_groupB">
-                    <Article />
-                    <Article />
-                    <Article />
-                    <Article />
+                    <Article imgUrl={blog02} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
+
+                    <Article imgUrl={blog03} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
+
+                    <Article imgUrl={blog04} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
+
+                    <Article imgUrl={blog05} date='Sep 26, 2021' title='GPT-3 and Open  AI is the future. Let us exlore how it is?' />
                 </div>
             </div>
         </div>
